7dd53bfd92bf60925f4f27d3678f9732280dc251
Dylan Pirrotta <dpirrotta9@hotmail.com>
2022-02-09 23:00:06 +0000

    Corrected some linting issues that were forgotten earlier

--- middle.js
+++ middle.js
@@ -19,25 +19,25 @@ const middle = function(array) {
   if (array.length < 3) return [];
 
   // Will be whole or decimal
-  let middleNum = array.length/2;
+  let middleNum = array.length / 2;
 
-  // If array.length even, return element at array.length/2.floor && .ceil
+  // If array.length even, return element at array[middleNum-1] and array[middleNum]
   if (array.length % 2 === 0) {
     const num1 = array[middleNum - 1];
     const num2 = array[middleNum];
     return [num1, num2];
-  } 
+  }
 
   // if array.length odd, return element at index array.length/2.floor
   if (array.length % 2 === 1) return [array[Math.floor(middleNum)]];
 
 };
 
-assertArraysEqual(middle([1]), []) // => []
-assertArraysEqual(middle([1, 2]), []) // => []
+assertArraysEqual(middle([1]), []); // => []
+assertArraysEqual(middle([1, 2]), []); // => []
 
-assertArraysEqual(middle([1, 2, 3]), [2]) // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]) // => [3]
+assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]) // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]) // => [3, 4]
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
