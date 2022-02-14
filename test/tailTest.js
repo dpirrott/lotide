64efe6ff2733b7c93fd6102704bcc6d1ae29c90f
Dylan Pirrotta <dpirrotta9@hotmail.com>
2022-02-14 22:27:30 +0000

    Used assert.deepEqual from chai instead of personal eqArrays

--- test/tailTest.js
+++ test/tailTest.js
@@ -4,12 +4,12 @@ const assert = require('chai').assert;
 
 describe('#tail', () => {
   it('returns [] when passed ["Dylan"]', () => {
-    assert.isTrue(eqArrays(tail(["Dylan"]), []));
+    assert.deepEqual(tail(["Dylan"]), []);
   });
   it('returns Pirrotta when passed ["Dylan", "Pirrotta"]', () => {
     assert.strictEqual(tail(["Dylan", "Pirrotta"]), "Pirrotta");
   });
   it('returns "Paul", "Pirrotta" when passed ["Dylan", "Paul", "Pirrotta"]', () => {
-    assert.isTrue(eqArrays(tail(["Dylan", "Paul", "Pirrotta"]), ["Paul", "Pirrotta"]));
+    assert.deepEqual(tail(["Dylan", "Paul", "Pirrotta"]), ["Paul", "Pirrotta"]);
   });
 });
